8feb943f98d7d049086c5c414d5f73919b3d6503
Leon Canales <tu-correo@ejemplo.com>
2026-05-09 07:15:45 +0000

    protocol: introduce federation integration layer

---
+++ vbc-federation-layer.js
@@ -0,0 +1,12 @@
+// VBC Federation Protocol Layer
+
+export function registerFederation(data){
+
+  return {
+    id:data.id,
+    name:data.name,
+    country:data.country,
+    verified:true
+  }
+
+}
